00dc35dc9f0e5ade3118408b373d026a95556130
Hieu <hieu21042003a@gmail.com>
2024-12-31 20:05:44 +0700

    Deploy to gh-pages

---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+/**
+ * Author: TrungQuanDev - Một Lập Trình Viên
+ * Channel: https://youtube.com/@trungquandev
+ * Created at: Feb 09, 2024
+ */
+
+import { useRef } from "react";
+import { Fireworks } from "@fireworks-js/react";
+import "./App.css";
+
+function App() {
+  const fireworksRef = useRef(null);
+  return (
+    <div className="container">
+      <div className="fireworks-area">
+        <Fireworks
+          ref={fireworksRef}
+          options={{
+            opacity: 0.7,
+            particles: 200,
+            explosion: 7,
+            intensity: 70,
+          }}
+        />
+      </div>
+
+      <div className="content">
+        <h1 className="title">
+          Happy New Year!
+          <br />
+          <h4>
+            Chúc Kim Anh và gia đình sang 2025 dồi dào sức khỏe, gặp nhiều may
+            mắn
+          </h4>
+          <div className="animate-color">
+            <div className="animate-color__item"></div>
+            <div className="animate-color__item"></div>
+            <div className="animate-color__item"></div>
+            <div className="animate-color__item"></div>
+          </div>
+        </h1>
+        <div className="author">
+          <a target="_blank">
+            <i className="">Riêng bạn Kim Anh thì bận ít thôi nhé !!!</i>
+          </a>
+        </div>
+        {/* Nếu muốn custom nâng cao thì sử dụng ref như ví dụ dưới đây mình làm nút Restart nhé */}
+        {/* <button onClick={() => fireworksRef?.current?.clear()}>Restart</button> */}
+      </div>
+    </div>
+  );
+}
+
+export default App;
